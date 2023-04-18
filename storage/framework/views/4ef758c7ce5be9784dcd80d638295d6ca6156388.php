e878b041be540e9a876b06df6fe4d6d73ef264d4
jarwonozt <jarwonoztaveiro@gmail.com>
2023-04-18 15:53:56 +0700

    update from areai

--- storage/framework/views/4ef758c7ce5be9784dcd80d638295d6ca6156388.php
+++
@@ -1,5 +0,0 @@
-<a href="/" class="mt-4">
-    <h1><strong>LOGIN APTII</strong></h1>
-    
-</a>
-<?php /**PATH /home/jarwonozt/Desktop/WEB/aptii/data/resources/views/vendor/jetstream/components/authentication-card-logo.blade.php ENDPATH**/ ?>
